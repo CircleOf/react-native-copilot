bbf584253edcb3f40f3231b3131cfff8199c0ae1
Andrey Guzyuk <necromant1k96@gmail.com>
2021-10-08 14:54:05 +0300

    Fix flickering

--- src/components/SvgMask.js
+++ src/components/SvgMask.js
@@ -120,12 +120,6 @@ class SvgMask extends Component<Props, State> {
                   fill={this.props.backdropColor}
                   fillRule="evenodd"
                   strokeWidth={1}
-                  d={this.props.svgMaskPath({
-                    size: this.state.size,
-                    position: this.state.position,
-                    canvasSize: this.state.canvasSize,
-                    step: this.props.currentStep,
-                  })}
                 />
               </Svg>
             )
